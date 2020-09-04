f3d334162b99ee3bc028f131c8098d5547c1d080
Nadilson Ferreira <nadilson@gmail.com>
2020-09-04 17:10:41 -0300

    Fixing interface change

--- pkg/adapter/fiber/fiber_adapter.go
+++ pkg/adapter/fiber/fiber_adapter.go
@@ -25,7 +25,7 @@ var (
 )
 
 type Params map[string]interface{}
-type Handler func(path string, handlers ...f.Handler) *f.Route
+type Handler func(path string, handlers ...f.Handler) f.Router
 type GetParams func(fiberRule *BindingRule,
 	businessService service.BusinessService,
 	c *f.Ctx) (Params, error)
